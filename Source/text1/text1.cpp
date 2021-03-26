35cdc8d6afe2cf58ede444d3e8297bc0350a4f33
unknown <you@example.com>
2021-03-26 16:20:14 +0800

    init

---
+++ Source/text1/text1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "text1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, text1, "text1" );
---
+++ Source/text1/text1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "text1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEXT1_API Atext1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
